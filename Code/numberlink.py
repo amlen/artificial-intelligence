5f2e5ae680d59303088235d8c449058635f7542a
Florian Thuin <florian.thuin@student.uclouvain.be>
2015-10-04 11:19:40 +0200

    nouveaux ajouts

--- Code/numberlink.py
+++ Code/numberlink.py
@@ -13,6 +13,7 @@ class NumberLink(Problem):
     def __init__(self, init):
         self.width =  0
         self.height = 0
+        self.lettersTab = {}
         with open(init, "r") as file:
             # Lecture du fichier
             data_read = file.read()
@@ -23,6 +24,17 @@ class NumberLink(Problem):
             self.grid.pop()
             self.width=len(self.grid[0])
             self.height=len(self.grid)
+            j=0
+            for line in self.grid:
+                for i in range(0, len(line)):
+                    if not (line[i]== "." or line[i] in self.lettersTab):
+                        self.lettersTab[line[i]]= [i, j, None, None]
+                    elif not line[i]==".":
+                        self.lettersTab[line[i]][2]=i
+                        self.lettersTab[line[i]][3]=j
+                j+=1
+
+            print("Nombre de lettres = " + repr(len(self.lettersTab)))
             print("Nombre de colonnes = " + repr(self.width))
             print("Nombre de lignes = " + repr(self.height))
 
@@ -45,6 +57,8 @@ class NumberLink(Problem):
         grid = self.stateToGrid(state)
         action = grid.pop().split(",")
         print("OLD ACTION =" + repr(action))
+        if self.pathIsComplete(state, action[2]):
+            print("##########TERMINE############")
         for d in directions:
             i = int(action[0]) + d[0]
             j = int(action[1]) + d[1]
@@ -54,7 +68,9 @@ class NumberLink(Problem):
                 line = grid[j]
                 newline = line[:i] + action[2] + line[i+1:]
                 grid[j] = newline
-                yield (repr(i) + "," + repr(j) + "," + action[2], self.gridToState(grid))
+                yield (repr(i) + "," + repr(j) + "," + action[2],
+                        self.gridToState(grid) + repr(i) + "," + repr(j)
+                        + "," + action[2])
 
     def gridToState(self, grid):
         state = ""
@@ -86,7 +102,26 @@ class NumberLink(Problem):
         for i in range(0, self.height):
             print(state[(i*self.width):(i+1)*self.width])
 
+    def pathIsComplete(self, state, letter):
+        start = self.lettersTab[letter][0:2]
+        end = self.lettersTab[letter][2:4]
+        grid= self.stateToGrid(state)
+        grid.pop() # pop the action
+        self.pathIsCompleteAux(grid, letter, start, end)
 
+    def pathIsCompleteAux(self, grid, letter, start, end):
+        if start[0]==end[0] and start[1]==end[1]:
+            return True
+        else:
+            grid[start[1]]=grid[start[1]][:start[0]] + "." + grid[start[1]][start[0]+1:]
+            for d in directions:
+                i = start[0]+d[0]
+                j = start[1]+d[1]
+                next = [j, i]
+                if inBounds(self.grid, next) and grid[j][i] == letter:
+                    self.pathIsCompleteAux(grid, letter, [i,j], end)
+        print("What does grid is ?" + repr(grid))
+        return False
 
 ######################
 # Auxiliary function #
@@ -132,7 +167,7 @@ def inBounds(grid, pos):
 
 problem=NumberLink(sys.argv[1])
 #problem.successor(problem.initial)
-for i in problem.successor(problem.initial):
+"""for i in problem.successor(problem.initial):
     print("i[1]=" + i[1])
     problem.printState(i[1])
     for j in problem.successor(i[1]+i[0]):
@@ -140,11 +175,11 @@ for i in problem.successor(problem.initial):
         problem.printState(j[1])
         for k in problem.successor(j[1]+j[0]):
             print("Third nodes")
-            problem.printState(k[1])
+            problem.printState(k[1])"""
 #example of bfs search
-###node=depth_first_graph_search(problem)
+node=depth_first_graph_search(problem)
 #example of print
-###path=node.path()
-###path.reverse()
-###for n in path:
-###    print(n.state) #assuming that the __str__ function of states output the correct format
+path=node.path()
+path.reverse()
+for n in path:
+    print(n.state) #assuming that the __str__ function of states output the correct format
