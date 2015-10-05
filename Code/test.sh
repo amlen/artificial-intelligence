8de562af6f5dced3e3b0c0b70a89e9b8e8bb0e59
Florian Thuin <florian.thuin@student.uclouvain.be>
2015-10-05 17:58:44 +0200

    ajout tests automatisés

---
+++ Code/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+python3.4 numberlink.py ../instances/easy.in > ../results/log_easy.txt
+python3.4 numberlink.py ../instances/level2m.in > ../results/log_level2m.txt
+python3.4 numberlink.py ../instances/level4m.in > ../results/log_level4m.txt
+python3.4 numberlink.py ../instances/level9m.in > ../results/log_level9m.txt
+python3.4 numberlink.py ../instances/level10m.in > ../results/log_level10m.txt
+python3.4 numberlink.py ../instances/level15m.in > ../results/log_level15m.txt
+python3.4 numberlink.py ../instances/level38s.in > ../results/log_level38s.txt
+python3.4 numberlink.py ../instances/level39s.in > ../results/log_level39s.txt
+python3.4 numberlink.py ../instances/path.in > ../results/log_path.txt
+python3.4 numberlink.py ../instances/wiki.in > ../results/log_wiki.txt
