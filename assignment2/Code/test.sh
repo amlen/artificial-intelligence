fa1237356ca35babc2cc286bcc1c1fe284b5689e
= <florian.thuin@student.uclouvain.be>
2015-10-15 15:50:02 +0200

    ajout du rapport

---
+++ assignment2/Code/test.sh
@@ -0,0 +1,6 @@
+python3.4 Sokoban.py benchsGiven/sokoInst01 > log_sokoInst01.txt
+python3.4 Sokoban.py benchsGiven/sokoInst02 > log_sokoInst02.txt
+python3.4 Sokoban.py benchsGiven/sokoInst07 > log_sokoInst07.txt
+python3.4 Sokoban.py benchsGiven/sokoInst08 > log_sokoInst08.txt
+python3.4 Sokoban.py benchsGiven/sokoInst15 > log_sokoInst15.txt
+
